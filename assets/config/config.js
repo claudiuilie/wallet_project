8d61008853f4de1104ccc4b46f5b1ca0163cff7b
claudiuilie <claudiu.ilie0322@gmail.com>
2019-09-01 19:49:10 +0100

    configs

--- assets/config/config.js
+++ assets/config/config.js
@@ -2,14 +2,14 @@
 class Options {
     constructor() {
         this.server =  {
-            host:'192.168.1.101',
+            host:'192.168.1.102',
                 port: 8000
         }
 
         this.mysql  =  {
-            host     : 'localhost',
-                user     : 'root',
-                password : '',
+            host     : '192.168.1.102',
+                user     : 'incomeapp',
+                password : 'Bulgaria188',
                 database : 'test'
         }
     }
