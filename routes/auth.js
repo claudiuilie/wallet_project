a63526987fbabe079a9456e0e104176f1610d62a
Claudiu-Teofil Ilie <t026iliec@rbro.rbg.cc>
2019-09-10 16:53:12 +0300

    hash

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,10 @@ const mysqlController = require('../assets/js/mysqlController');
 const options = require('../assets/config/config');
 const express = require('express');
 const config = new options();
+const passwordHash = require('password-hash');
+
+// console.log(passwordHash.generate('test')); // generate hash
+
 let router = express.Router();
 let mysql = new mysqlController(config.mysql);
 
@@ -13,16 +17,20 @@ router.post('/', (req, res, next) => {
     let username = req.body.username;
     let password = req.body.password;
     if (username && password) {
-        mysql.select('accounts', {'username': username, 'password': password}, (error, results) => {
+        mysql.select('accounts', {'username': username}, (error, results) => {
             if(error) {
                 return next(error);
             }
             if (results.length > 0) {
-                req.session.loggedin = true;
-                req.session.username = username;
-                res.redirect('/home');
+                if(passwordHash.verify(password, results[0].password)) {
+                    req.session.loggedin = true;
+                    req.session.username = username;
+                    res.redirect('/home');
+                } else {
+                    return next('Incorrect Password!');
+                }
             } else {
-                return next('Incorrect Username and/or Password!');
+                return next('Incorrect Username!');
             }
             res.end();
         });
@@ -30,3 +38,8 @@ router.post('/', (req, res, next) => {
 });
 
 module.exports = router;
+
+// console.log(passwordHash.verify('bulgaria188', 'sha1$f15aa13a$1$d21ad877e10446d0bb17c099b7cc1753e27911b2')); // true
+// console.log(passwordHash.verify('Password0', 'sha1$f15aa13a$1$d21ad877e10446d0bb17c099b7cc1753e27911b2')); // false
+
+
