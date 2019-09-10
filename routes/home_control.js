f648bd9d1653923d78d21a87c7148c5a873d9fe0
Claudiu-Teofil Ilie <t026iliec@rbro.rbg.cc>
2019-09-10 16:01:31 +0300

    home assistant

--- routes/home_control.js
+++ routes/home_control.js
@@ -1,11 +1,30 @@
 const express = require('express');
 let router = express.Router();
+var jsonData = {
+    "tempSensors": [
+        { "temp": 20, "badge": "Bedroom Temp :", "unit": "°C" },
+        { "temp": 20, "badge": "Livin Temp :", "unit": "°C" },
+        { "temp": 20, "badge": "Kitchen Temp :", "unit": "°C" },
+        { "temp": 20, "badge": "Bathroom Temp :", "unit": "°C" }],
+    "humiditySensors": [
+        { "humidity": 20, "badge": "Bedroom humidity :", "unit": "%" },
+        { "humidity": 20, "badge": "Living humidity :", "unit": "%" },
+        { "humidity": 20, "badge": "Kitchen humidity :", "unit": "%" },
+        { "humidity": 20, "badge": "Bathroom humidity :", "unit": "%" }],
+    "relayStatus":
+        {
+            "bedroom":
+                { "status": true, "trigger": "<a href='/?relay1on'>on<a>" }
+        }
+}
 
 router.get('/',(req,res) => {
     if (!req.session.loggedin) res.redirect('/auth');
 
     else {
-        res.render('home_control');
+        res.render('home_control',{
+            sensors: jsonData
+        });
     }
 });
 
