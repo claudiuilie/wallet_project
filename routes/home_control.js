553326224e581ded6ed17964cca0dbbe558dc7f5
Claudiu-Teofil Ilie <t026iliec@rbro.rbg.cc>
2019-09-16 16:14:40 +0300

    'ardu'

--- routes/home_control.js
+++ routes/home_control.js
@@ -1,29 +1,25 @@
 const express = require('express');
-let router = express.Router();
-var jsonData = {
-    "tempSensors": [
-        { "temp": 20, "badge": "Bedroom Temp :", "unit": "°C" },
-        { "temp": 20, "badge": "Livin Temp :", "unit": "°C" },
-        { "temp": 20, "badge": "Kitchen Temp :", "unit": "°C" },
-        { "temp": 20, "badge": "Bathroom Temp :", "unit": "°C" }],
-    "humiditySensors": [
-        { "humidity": 20, "badge": "Bedroom humidity :", "unit": "%" },
-        { "humidity": 20, "badge": "Living humidity :", "unit": "%" },
-        { "humidity": 20, "badge": "Kitchen humidity :", "unit": "%" },
-        { "humidity": 20, "badge": "Bathroom humidity :", "unit": "%" }],
-    "relayStatus":
-        {
-            "bedroom":
-                { "status": true, "trigger": "<a href='/?relay1on'>on<a>" }
-        }
-}
+const request = require('request');
+const options = require('../assets/config/config');
 
-router.get('/',(req,res) => {
+let config = new options();
+let router = express.Router();
+let jsonData;
+
+router.get('/',(req,res, next) => {
     if (!req.session.loggedin) res.redirect('/auth');
 
     else {
-        res.render('home_control',{
-            sensors: jsonData
+        request(config.arduino.host, function (error, response, body) {
+            if(error) {
+                return next(error);
+            }
+            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+            console.log('body:', body); // Print the HTML for the Google homepage.
+            jsonData = body;
+            res.render('home_control',{
+                sensors: jsonData
+            });
         });
     }
 });
