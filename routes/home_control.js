b84dc406442a87fb33c8b7d820ce57b51b55be44
claudiuilie <claudiu.ilie0322@gmail.com>
2019-09-17 21:45:12 +0300

    ardu

--- routes/home_control.js
+++ routes/home_control.js
@@ -10,72 +10,70 @@ router.get('/',(req,res, next) => {
     if (!req.session.loggedin) res.redirect('/auth');
 
     else {
-        // request(config.arduino.host, function (error, response, body) {
-        //     if(error) {
-        //         return next(error);
-        //     }
-        //     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        //     console.log('body:', body); // Print the HTML for the Google homepage.
-        //     jsonData = body;
-        //     res.render('home_control',{
-        //         sensors: jsonData
-        //     });
-        // });
+        request(config.arduino.host, function (error, response, body) {
+            if(error) {
+                return next(error);
+            }
+            jsonData = JSON.parse(body);
+            res.render('home_control',{
+                sensors: jsonData
+            });
+        });
 
         /// manual data ///
         // temp: variabila cu temperatura senzorului
-        jsonData = {
-            "sensors":[
-                {
-                    "temp":25,
-                    "tempProcent": (25/50)*100,
-                    "humidity": 50,
-                    "icon": "fas fa-bed",
-                    "relays": []
-                },
-                {
-                    "temp":30,
-                    "tempProcent": (30/50)*100,
-                    "humidity": 50,
-                    "icon": "fas fa-couch",
-                    "relays": [
-                        {
-                            "status" : false,
-                            "name" : "living lights"
-                        }
-                    ]
-                },
-                {
-                    "temp":10,
-                    "tempProcent": (10/50)*100,
-                    "humidity": 50,
-                    "icon": "fas fa-utensils",
-                    "relays": [
-                        {
-                            "status" : false,
-                            "name" : "kitchen lights"
-                        }
-                    ]
-                },
-                {
-                    "temp":45,
-                    "tempProcent": (45/50)*100,
-                    "humidity": 50,
-                    "icon": "fas fa-bath",
-                    "relays": []
-                }
-            ],
-            "relays": [
-                {
-                    "name": "kitchen lights",
-                    "icon":"fas fa-utensils",
-                    "status": true
-                }
-            ]
-        };
-        res.render('home_control',{
-            sensors: jsonData
-        });
+        // jsonData = {
+        //     "sensors":[
+        //         {
+        //             "temp":25,
+        //             "tempProcent": (25/50)*100,
+        //             "humidity": 50,
+        //             "icon": "fas fa-bed",
+        //             "relays": []
+        //         },
+        //         {
+        //             "temp":30,
+        //             "tempProcent": (30/50)*100,
+        //             "humidity": 50,
+        //             "icon": "fas fa-couch",
+        //             "relays": [
+        //                 {
+        //                     "status" : false,
+        //                     "name" : "living lights"
+        //                 }
+        //             ]
+        //         },
+        //         {
+        //             "temp":10,
+        //             "tempProcent": (10/50)*100,
+        //             "humidity": 50,
+        //             "icon": "fas fa-utensils",
+        //             "relays": [
+        //                 {
+        //                     "status" : false,
+        //                     "name" : "kitchen lights"
+        //                 }
+        //             ]
+        //         },
+        //         {
+        //             "temp":45,
+        //             "tempProcent": (45/50)*100,
+        //             "humidity": 50,
+        //             "icon": "fas fa-bath",
+        //             "relays": []
+        //         }
+        //     ],
+        //     "relays": [
+        //         {
+        //             "name": "kitchen lights",
+        //             "icon":"fas fa-utensils",
+        //             "status": true
+        //         }
+        //     ]
+        // };
+        // res.render('home_control',{
+        //     sensors: jsonData
+        // });
     }
 });
 
